b830743d02dc47e84aee1b27d97d68419f63b1f8
pinhaosama <gabrielpinhao1@gmail.com>
2023-10-04 20:53:37 -0700

    cart update

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,19 @@
-$('.sp-icon').click(() => {
-    alert('wow');
-})
+var id = null;
+
+function toggleCart() {
+    if($("#cartCheck").checked == false) {
+        var pos = 0;
+        clearInterval(id);
+        id = setInterval(frame, 10);
+        function frame() {
+            if (pos == 300) {
+                clearInterval(id);
+            } else {
+                pos++;
+                $('.cart').css('height', pos+'px');
+            }
+        }
+    } else {
+        alert('bah pia');
+    }
+}
